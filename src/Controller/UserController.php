86dbfb2c868d0a1dd79a32a6f61294e7318655b3
BidaudA <antoine.bidaud@yahoo.fr>
2019-09-11 16:56:05 +0200

    security.yaml & UserController.php

---
+++ src/Controller/UserController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class UserController extends Controller
+{
+    /**
+     * @Route("/connexion", name="security_login")
+     */
+    public function index(AuthenticationUtils $authenticationUtils)
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('user/index.html.twig', [
+            'controller_name' => 'UserController',
+            'last_username'=>$lastUsername,
+            'error'=>$error
+        ]);
+    }
+
+    public function addUser(Request $request,UserPasswordEncoderInterface $passwordEncoder,EntityManagerInterface $em)
+}
