e7b6e1c5edad4c0e62f13dc92a5b5d2a5c7dd578
Victor Dittiere <victor.dittiere@icloud.com>
2019-09-12 12:27:54 +0200

    Edit Entity user

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -23,7 +23,7 @@ class User implements UserInterface
     private $name;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, unique=true)
      */
     private $email;
 
