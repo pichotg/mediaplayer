6841a811bbba4770034094230bde31f7ce32e5de
Victor Dittiere <victor.dittiere@icloud.com>
2019-09-12 15:02:09 +0200

    Media Player (finished)

--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,22 @@
 $(document).ready( function () {
 
-    var mediaTable = $('#mediaTable').DataTable({
+    let mediaTable = $('#mediaTable').DataTable({
         'searching': false,
     });
 
-    mediaTable.on('click','tr',function() {
-        var data = mediaTable.row( this ).data();
+    $('#playMediaModal').on('show.bs.modal', function (event) {
+        let button = $(event.relatedTarget);
+        let name = button.data('name');
+        let type = button.data('type');
+        let path = button.data('path');
 
-        $('#modalTitle').text(data[1]);
-        if(data[7] == 'Audio'){
-            $('#modalBody').html('<audio controls><source src="./files/media/' + data[3] + '" type="audio/mp3">Your browser does not support the audio element.</audio>');
-        } else if (data[7] == 'Video') {
-            $('#modalBody').html('<video width="320" height="240" controls><source src="./files/media/' + data[3] + '" type="audio/ogg">Your browser does not support the video element.</video>');
+        var modal = $(this);
+        modal.find('.modal-title').html(name);
+        if(type == 'Audio'){
+            modal.find('.modal-body').html('<audio class="w-100" controls><source src="' + path + '" type="audio/mp3">Your browser does not support the audio element.</audio>');
+        } else if (type == 'Video') {
+            modal.find('.modal-body').html('<video class="w-100" width="320" height="240" controls><source src="' + path + '" type="audio/ogg">Your browser does not support the video element.</video>');
         }
-
-        $('#playMediaModal').modal().show();
-    });
-
-    var genreTable = $('#genreTable').DataTable({
-        'searching': false,
-    });
+    })
 
 } );
